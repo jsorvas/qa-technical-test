3e2efdbb356f503b49db97b62f107d3247b13442
jsorvas <59997694+jsorvas@users.noreply.github.com>
2026-06-09 22:25:38 +0200

    Expand login tests with edge cases
    Added edge case tests for login functionality, including scenarios for missing credentials and incorrect username/password combinations

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -12,3 +12,99 @@ test.describe("Login Tests", () => {
     expect(isLoggedIn).toBeTruthy();
   });
 });
+
+test.describe("Edge cases", () => {
+  test("should login unsuccessfully without user & password", async ({
+    page,
+  }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("", "");
+
+    const isLoggedIn = await loginPage.isLoggedIn();
+    expect(isLoggedIn).toBeFalsy();
+
+    //Vérifier le message d'erreur
+    const errorMessage: string = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain("Username is required");
+  });
+
+  test("should login unsuccessfully without user", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("", "secret_sauce");
+
+    const isLoggedIn = await loginPage.isLoggedIn();
+    expect(isLoggedIn).toBeFalsy();
+
+    //Vérifier le message d'erreur
+    const errorMessage: string = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain("Username is required");
+  });
+
+  test("should login unsuccessfully without password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("standard_user", "");
+
+    const isLoggedIn = await loginPage.isLoggedIn();
+    expect(isLoggedIn).toBeFalsy();
+
+    //Vérifier le message d'erreur
+    const errorMessage: string = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain("Password is required");
+  });
+
+  test("should login unsuccessfully with wrong user & password", async ({
+    page,
+  }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("xstandard_user", "7secret_sauce");
+
+    const isLoggedIn = await loginPage.isLoggedIn();
+    expect(isLoggedIn).toBeFalsy();
+
+    //Vérifier le message d'erreur
+    const errorMessage: string = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain(
+      "Username and password do not match any user in this service",
+    );
+  });
+
+  test("should login unsuccessfully with wrong user", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("xstandard_user", "secret_sauce");
+
+    const isLoggedIn = await loginPage.isLoggedIn();
+    expect(isLoggedIn).toBeFalsy();
+
+    //Vérifier le message d'erreur
+    const errorMessage: string = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain(
+      "Username and password do not match any user in this service",
+    );
+  });
+
+  test("should login unsuccessfully with wrong password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login("standard_user", "7secret_sauce");
+
+    const isLoggedIn = await loginPage.isLoggedIn();
+    expect(isLoggedIn).toBeFalsy();
+
+    //Vérifier le message d'erreur
+    const errorMessage: string = await loginPage.getErrorMessage();
+    expect(errorMessage).toContain(
+      "Username and password do not match any user in this service",
+    );
+  });
+});
